fc8edbdcfcb593294e237236fcf2db79a05c28b3
Clement Farabet <clement.farabet@gmail.com>
2011-08-26 01:23:08 -0400

    Starting to extend nn.Module to support graph-like operators.

--- packages/nn/Module.lua
+++ packages/nn/Module.lua
@@ -46,3 +46,43 @@ function Module:clone(...)
    end
    return clone
 end
+
+function Module:__call__(...)
+   local args = {...}
+   local module
+   if select('#',...) == 1 then -- sequential
+      module = nn.Sequential()
+      if torch.typename(args[1]) == 'nn.Sequential' then
+         module = args[1]
+         module:add(self)
+      else
+         module:add(args[1])
+         module:add(self)
+      end
+   elseif select('#',...) > 1 then -- parallel
+      module = nn.Sequential()
+      local par = nn.ParallelTable()
+      for i = 1,select('#',...) do
+         par:add(args[i])
+      end
+      module:add(par)
+      module:add(nn.JoinTable(1))
+      module:add(self)
+   end
+   return module
+end
+
+function Module:__index__(key)
+   if type(key)=='number' then
+      local module
+      if torch.typename(self) == 'nn.Sequential' then
+         module = self
+      else
+         module = nn.Sequential()
+         module:add(self)
+      end
+      module:add(nn.Narrow(1,key,1))
+      return module, true
+   end
+   return rawget(self,key)
+end
