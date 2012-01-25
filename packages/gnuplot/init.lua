1f2beb94c757c257bb7408ab9c3bfef22bd1aef7
Soumith Chintala <soumith@gmail.com>
2012-01-25 18:26:20 -0500

    separated TH from torch-7 and modified CMakeLists for openmp and cuda to install the corresponding headers

--- packages/gnuplot/init.lua
+++
@@ -1,7 +0,0 @@
-require 'torch'
-require 'lab'
-
-gnuplot = {}
-torch.include('gnuplot','gnuplot.lua')
-
---plot = gnuplot
--- packages/torch/TensorMath.c
+++
@@ -1,8 +0,0 @@
-#include "general.h"
-
-#define torch_TensorMath_(NAME) TH_CONCAT_4(torch_,Real,TensorMath_,NAME)
-#define torch_Tensor_id TH_CONCAT_3(torch_,Real,Tensor_id)
-#define STRING_torchTensor TH_CONCAT_STRING_3(torch.,Real,Tensor)
-
-#include "generic/TensorMath.c"
-#include "THGenerateAllTypes.h"
